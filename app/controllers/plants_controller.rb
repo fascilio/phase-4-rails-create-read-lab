84cb9075558a7b184d3abc06440210d4ac9cd95a
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-06-22 15:45:21 +0300

    lab passed

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,37 @@
+class PlantsController < ApplicationController
+    def index
+      plants = Plant.all
+      render json: plants
+    end
+
+    def show
+      plant = Plant.find(params[:id])
+      render json: plant
+    end
+
+    # def create
+    #   plant_params = params.require(:plant).permit(:name, :image, :price)
+    #   plant = Plant.new(plant_params)
+      
+    #   if plant.save
+    #     render json: plant, status: :created
+    #   else
+    #     render json: { error: plant.errors.full_messages }, status: :unprocessable_entity
+    #   end
+    # end
+    def create
+      plant = Plant.new(plant_params)
+      if plant.save
+        render json: plant, status: :created
+      else
+        render json: { error: plant.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+    
+      private
+    
+    def plant_params
+      params.permit(:name, :image, :price)
+    end
+      
+end
